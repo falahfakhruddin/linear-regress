db636e0e113b06f0abef080f79fdab379d67837e
Falah Fakhruddin <falah.fakhruddin@gdn-commerce.com>
2017-11-10 06:01:05 +0700

    Update code

--- linier_reg.py
+++ linier_reg.py
@@ -5,7 +5,6 @@ Created on Thu Nov  9 21:41:30 2017
 @author: falah.fakhruddin
 """
 import sys
-import pandas as pd 
 import numpy as np
 
 def sum_array(array1):
@@ -24,13 +23,15 @@ def calculate_coefficient(sum1, sum2, n, m):
     y_mean = sum2/n
     return y_mean-(m*x_mean)
     
-def import_dataset(a):  
+def import_dataset(a, c):  
     X= a[:, 0]
     Y= a[:, 1]
     grad  = calculate_gradient(sum_array(X), sum_array(Y), sum_array_product(X,Y), sum_array_product(X,X), len(X) )
     coeff = calculate_coefficient(sum_array(X), sum_array(Y), len(X), grad)
-    print (grad)
-    print (coeff)
+    with open (temp[2], 'a') as hasil:
+        hasil.writelines("Hasil Perhitungan data ke- %d : \n" % c)
+        hasil.writelines("Besar Gradien : %f \n" % grad)
+        hasil.writelines("Besar koefisien: %f \n\n" % coeff)
     
 if __name__ == '__main__':
 #Importing Dataset
@@ -54,11 +55,15 @@ if __name__ == '__main__':
                     results2.append(line.strip().split(','))
     T1 = [list(map(float, x)) for x in results1]
     T2 = [list(map(float, x)) for x in results2]
-          
+               
     dataset1= np.array(T1)
     dataset2= np.array(T2)
     
-    import_dataset(dataset1)
-    import_dataset(dataset2)
+    open(temp[2],"w").close() 
+    
+    i=1
+    import_dataset(dataset1, 1)
+    i=2
+    import_dataset(dataset2, 2)
 
     
