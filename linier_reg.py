795f549a2a412c38edef11bd3ea910881e83f05a
Falah Fakhruddin <falah.fakhruddin@gdn-commerce.com>
2017-11-09 23:25:43 +0700

    Adding new file

---
+++ linier_reg.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  9 21:41:30 2017
+
+@author: falah.fakhruddin
+"""
+def sum_array(array1):
+    return sum(array1)
+    
+def sum_array_product(array1,array2):
+    return sum(array1*array2)
+
+def calculate_gradient(sum1, sum2, sum3, sum4, n):
+    sxy = sum3 - (sum1*sum2/n)
+    sxx = sum4 - (sum1*sum1/n)
+    return sxy/sxx
+    
+def calculate_coefficient(sum1, sum2, n, m):
+    x_mean = sum1/n
+    y_mean = sum2/n
+    return y_mean-(m*x_mean)
+    
+def import_dataset(a):  
+    X= a[:, 0]
+    Y= a[:, 1]
+    grad  = calculate_gradient(sum_array(X), sum_array(Y), sum_array_product(X,Y), sum_array_product(X,X), len(X) )
+    coeff = calculate_coefficient(sum_array(X), sum_array(Y), len(X), grad)
+    print (grad)
+    print (coeff)
+    
+if __name__ == '__main__':
+#Importing Dataset
+    temp = sys.argv
+
+    print(int(data.getTemp()))
+    results1 = []
+    results2 = []
+
+    condition = False
+    with open('data3.txt') as inputfile:
+        for line in inputfile:
+            if condition == False:
+                if line != '---\n':
+                    results1.append(line.strip().split(','))
+                 
+                else:
+                    condition = True
+            else:
+                    results2.append(line.strip().split(','))
+    T1 = [list(map(float, x)) for x in results1]
+    T2 = [list(map(float, x)) for x in results2]
+    dataset1= np.array(T1)
+    dataset2= np.array(T2)
+    
+    import_dataset(dataset1)
+    import_dataset(dataset2)
+
+    
