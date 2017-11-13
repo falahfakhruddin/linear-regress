a6ef534d004ebac6901a019ea108c70b5b069e94
Falah Fakhruddin <falah.fakhruddin@gdn-commerce.com>
2017-11-13 18:59:03 +0700

    update code for title dataset

--- linier_reg.py
+++ linier_reg.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Nov  9 21:41:30 2017
+Created on Fri Nov 10 08:53:13 2017
 
 @author: falah.fakhruddin
 """
+
 import sys
 import numpy as np
 
@@ -23,13 +24,13 @@ def calculate_coefficient(sum1, sum2, n, m):
     y_mean = sum2/n
     return y_mean-(m*x_mean)
     
-def import_dataset(a, c):  
+def import_dataset(a, b, c):  
     X= a[:, 0]
     Y= a[:, 1]
     grad  = calculate_gradient(sum_array(X), sum_array(Y), sum_array_product(X,Y), sum_array_product(X,X), len(X) )
     coeff = calculate_coefficient(sum_array(X), sum_array(Y), len(X), grad)
-    with open (temp[2], 'a') as hasil:
-        hasil.writelines("Hasil Perhitungan data ke- %d : \n" % c)
+    with open (temp[-1], 'a') as hasil:
+        hasil.writelines("Hasil Perhitungan dataset %s" % b)
         hasil.writelines("Besar Gradien : %f \n" % grad)
         hasil.writelines("Besar koefisien: %f \n\n" % coeff)
     
@@ -39,31 +40,27 @@ if __name__ == '__main__':
 
     print(temp)
     
-    results1 = []
-    results2 = []
-
-    condition = False
-    with open(temp[1]) as inputfile:
-        for line in inputfile:
-            if condition == False:
-                if line != '---\n':
-                    results1.append(line.strip().split(','))
-                 
-                else:
-                    condition = True
-            else:
-                    results2.append(line.strip().split(','))
-    T1 = [list(map(float, x)) for x in results1]
-    T2 = [list(map(float, x)) for x in results2]
+    results = []
+    condition = True
+    i = 1
+    j = 1
+    
+    open(temp[-1],"w").close() 
+    
+    for j in range (1,len(temp)):
+        with open(temp[j]) as inputfile:
+            for line in inputfile:
+                                
+                if condition == True:
+                   nama=line
+                   condition = False
                
-    dataset1= np.array(T1)
-    dataset2= np.array(T2)
-    
-    open(temp[2],"w").close() 
-    
-    i=1
-    import_dataset(dataset1, 1)
-    i=2
-    import_dataset(dataset2, 2)
-
-    
+                elif line == '---\n' or line == 'eof': 
+                    T1 = [list(map(float, x)) for x in results]
+                    import_dataset(np.array(T1),nama, i)
+                    i+=1
+                    results = []
+                    condition = True
+                    
+                else: 
+                    results.append(line.strip().split(','))
