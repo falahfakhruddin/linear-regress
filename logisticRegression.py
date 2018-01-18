f56f1f75a1bae1f834c6e593c3d1cab400a23f41
Falah Fakhruddin <falah.fakhruddin@gdn-commerce.com>
2018-01-18 10:28:46 +0700

    seperate between testing and predict function

--- logisticRegression.py
+++ logisticRegression.py
@@ -17,14 +17,14 @@ class LogisticRegression ():
 
       def sigmoid(self, scores):
           return 1 / (1 + np.exp(-scores))
-      
+
       def training(self, features, target):
-          #adding extra features for coefficient parameter  
+          #adding extra features for coefficient parameter
           if self.intercept:
               intercept = np.ones((features.shape[0], 1))
-              features = np.hstack((intercept, features)) 
-              
-          self.uniqueTarget = np.unique(target) #get unique value of target 
+              features = np.hstack((intercept, features))
+
+          self.uniqueTarget = np.unique(target) #get unique value of target
           self.listWeights=[]
 
           for kind in range (len(self.uniqueTarget)): #looping for each unique target
@@ -33,34 +33,37 @@ class LogisticRegression ():
                 dfTarget.set_value(dfTarget[0]!= self.uniqueTarget[kind],[0],0)
                 dfTarget.set_value(dfTarget[0]== self.uniqueTarget[kind],[0],1)
                 numericalTarget = dfTarget.iloc[:,-1].values.astype(int)
-                
+
                 #initiate weights value
                 weights = np.zeros(features.shape[1])
-                
+
                 #gradient descent step
                 for step in range(self.num_steps):
                     scores = np.dot(features, weights) #calculate prediction value
                     predictions = self.sigmoid(scores) #transform with sigmoid function
-            
+
                     # delta error of real value and predict value
                     output_error_signal = numericalTarget - predictions
-                    
+
                     gradient = np.dot(features.T, output_error_signal) #value of derivative function for each parameter
                     weights += self.learning_rate * gradient #update weights
                 self.listWeights.append(weights)
 
-          return self.listWeights  
-         
-      def predict(self, features, target):
+          return self.listWeights
+
+      def predict(self, features, weights=None):
+          if weights == None:
+              weights = self.listWeights
+
           final_scores_list = []
           prediction = []
 
           #calculate the scores in test set
           for kind in range(len(self.uniqueTarget)):
                 final_scores = np.dot(np.hstack((np.ones((features.shape[0], 1)),
-                                       features)), self.listWeights[kind])
+                                       features)), weights[kind])
                 final_scores_list.append(final_scores)
-                
+
           #predict the label from scores
           for set in range (features.shape[0]):
                 predict_dictionary = {}
@@ -71,6 +74,14 @@ class LogisticRegression ():
           prediction = np.array(prediction)
           print("\nPrediction :")
           print (prediction)
+          return prediction
+
+      def testing(self, features, target, weights=None):
+          if weights == None:
+              weights = self.listWeights
+
+          #get prediction
+          prediction = self.predict(features, weights)
 
           #calculate error
           error = 0
@@ -81,14 +92,14 @@ class LogisticRegression ():
           error = float(error*100/len(target))
 
           return float(error)
-          
+
 if __name__ == "__main__":
       def playtennis() :
             client = MongoClient()
             db=client.newdb
             collection=db.playtennis.find()
             df =pd.DataFrame(list(collection))
-            del df['_id']  
+            del df['_id']
             print (df)
             target = df['play'].values.astype(str)
             del df['play']
@@ -96,7 +107,7 @@ if __name__ == "__main__":
             features = df.iloc[:,:].values
             print (df)
             return [features, target]
-      
+
       def iris() :
             client = MongoClient()
             db=client.newdb
@@ -107,26 +118,26 @@ if __name__ == "__main__":
             features = df.iloc[:,:-1].values.astype(float)
             target = df.iloc[:,-1].values.astype(str)
             return [features, target]
-      
+
       temp=sys.argv
-      #Load data and Preperation Data            
-      
+      #Load data and Preperation Data
+
       #Training Step
 
       dataset = temp[-1]
-      
+
       if dataset == "playtennis":
             [features, target] = playtennis()
-      else:     
+      else:
             [features, target] = iris()
-      
+
       #Training Step
       log_reg = LogisticRegression(num_steps=50000, learning_rate=5e-5)
       weights = log_reg.training(features, target)
 
       print ("Weights:")
       print (weights)
-      
+
       #Testing Step
-      error = log_reg.predict(features, target)
-      print ("\n Error : %i" %error +"%")
+      error = log_reg.testing(features, target)
+      print ("\n Error : %f" %error +"%")
