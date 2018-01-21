0dacd09a56b71b4fdcacd75344dd65269506b3a6
falahfakhruddin <falahfakhruddin@hotmail.com>
2018-01-21 16:35:58 +0700

    update database

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ from LogisticRegression import LogisticRegression
 from RegressionMainCode import MultiVariateRegression
 
 db = DatabaseConnector()
-list_db = db.get_collection("playtennis", "play")
+list_db = db.get_collection("homeprice", "Price")
 df =list_db[0]
 target = list_db[1]
 header = list(df)
 bias = ['bias']
 header = bias + header
+
+#import collection to db
+import pandas as pd
+import tools as tl
+homeprice = pd.read_csv("homeprice.txt")
+json_homperice = tl.transform_dataframe_json(homeprice)
+db.import_collection(json_homperice, "homeprice")
