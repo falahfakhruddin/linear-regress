1bcce0881b4329a8a5b55ade79d525f06dee3060
falahfakhruddin <falahfakhruddin@hotmail.com>
2018-01-21 21:22:24 +0700

    update code

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 from DatabaseConnector import DatabaseConnector
 from LogisticRegression import LogisticRegression
 from RegressionMainCode import MultiVariateRegression
-
-db = DatabaseConnector()
-list_db = db.get_collection("homeprice", "Price")
-df =list_db[0]
-target = list_db[1]
-header = list(df)
-bias = ['bias']
-header = bias + header
-
+import numpy as np
+import pandas as pd
+"""
 #import collection to db
 import pandas as pd
 import tools as tl
 homeprice = pd.read_csv("homeprice.txt")
 json_homperice = tl.transform_dataframe_json(homeprice)
 db.import_collection(json_homperice, "homeprice")
+"""
+
+db = DatabaseConnector()
+list_db = db.get_collection("homeprice", "Price", type="regression")
+df =list_db[0]
+target = list_db[1]
+header = list(df)
+bias = ['bias']
+header = bias + header
+
+#extract feature
+features = np.array(df)
+
+model = MultiVariateRegression()
+weights = model.training(features, target)
+weights.shape
+weight_frame = pd.DataFrame(weights.reshape(1,len(weights)))
