99107371ca3029fdd9ca008baa7ef87eb577415f
Falah Fakhruddin <falah.fakhruddin@gdn-commerce.com>
2018-01-23 10:52:32 +0700

    update code

--- main.py
+++ main.py
@@ -28,9 +28,3 @@ weights = model.training(features, target)
 
 weight_frame = pd.DataFrame(weights.reshape(1, len(weights)), columns=header)
 weights_json = tl.transform_dataframe_json(weight_frame)
-
-from Abstraction import AbstractML
-from ujicoba import aselos
-
-lose = aselos()
-print(lose.training(2))
