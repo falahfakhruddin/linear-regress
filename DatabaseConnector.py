c7415a693603ae27ee399a3dbe1fa182ea464e03
Falah Fakhruddin <falah.fakhruddin@gdn-commerce.com>
2018-01-23 11:25:59 +0700

    update code

--- DatabaseConnector.py
+++ DatabaseConnector.py
@@ -9,32 +9,6 @@ from pymongo import MongoClient
 import pandas as pd
 
 class DatabaseConnector():
-    def playtennis():
-        client = MongoClient()
-        db = client.newdb
-        collection = db.playtennis.find()
-        df = pd.DataFrame(list(collection))
-        del df['_id']
-        print(df)
-        target = df['play'].values.astype(str)
-        del df['play']
-        df = pd.get_dummies(df)
-        features = df.iloc[:, :].values
-        print(df)
-        return [features, target]
-
-
-    def iris():
-        client = MongoClient()
-        db = client.newdb
-        collection = db.irisdataset.find()
-        df = pd.DataFrame(list(collection))
-        del df['_id']
-        print(df)
-        features = df.iloc[:, :-1].values.astype(float)
-        target = df.iloc[:, -1].values.astype(str)
-        return [features, target]
-
     def get_collection(self, datafile, label, type='classification', database='newdb', dummies='no'): #get dataframe
         client = MongoClient()
         db = client[database]
@@ -97,4 +71,32 @@ from pprint import pprint
 data = json.load(open('homeprice.txt'))
 
 pprint(data)
+
+
+
+    def playtennis():
+        client = MongoClient()
+        db = client.newdb
+        collection = db.playtennis.find()
+        df = pd.DataFrame(list(collection))
+        del df['_id']
+        print(df)
+        target = df['play'].values.astype(str)
+        del df['play']
+        df = pd.get_dummies(df)
+        features = df.iloc[:, :].values
+        print(df)
+        return [features, target]
+
+
+    def iris():
+        client = MongoClient()
+        db = client.newdb
+        collection = db.irisdataset.find()
+        df = pd.DataFrame(list(collection))
+        del df['_id']
+        print(df)
+        features = df.iloc[:, :-1].values.astype(float)
+        target = df.iloc[:, -1].values.astype(str)
+        return [features, target]
 """
