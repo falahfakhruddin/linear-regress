4e147b7e4d91f8525d34186ef4c621d103e6c9da
Falah Fakhruddin <falah.fakhruddin@gdn-commerce.com>
2018-02-05 17:24:33 +0700

    update code

--- DatabaseConnector.py
+++ DatabaseConnector.py
@@ -8,9 +8,11 @@ Created on Tue Dec  5 15:14:53 2017
 from pymongo import MongoClient
 import pandas as pd
 import datetime
+from mongoengine import *
+
 
 class DatabaseConnector():
-    def get_collection(self, datafile, label, type='classification', database='newdb', dummies='no'): #get dataframe
+    def get_collection(self, datafile, label, type='classification', database='newdb', dummies='no'):  # get dataframe
         client = MongoClient()
         db = client[database]
         collection = db[datafile].find()
@@ -23,7 +25,7 @@ class DatabaseConnector():
             target = df[label].values.astype(float)
         del df[label]
 
-        if dummies =='yes':
+        if dummies == 'yes':
             features = pd.get_dummies(df)
             header = list(features)
             features = features.values
@@ -34,20 +36,28 @@ class DatabaseConnector():
 
         return features, target, header,
 
-    def import_collection(self, jsonfile, collection, database='newdb'): #upload json file into database
+    def export_collection(self, jsonfile, collection, database='newdb'):  # upload json file into database
         client = MongoClient()
         db = client[database]
         upload = db[collection]
         if type(jsonfile) == list:
-            result = upload.insert_many(jsonfile).inserted_ids
+            return upload.insert_many(jsonfile).inserted_ids
         elif type(jsonfile) == dict:
-            result = upload.insert_one(jsonfile).inserted_id
+            return upload.insert_one(jsonfile).inserted_id
 
-        return result
 
-"""
+class SaveModel(Document):
+    dataset = StringField(max_length=50, required=True)
+    algorithm = StringField(max_length=50, required=True)
+    preprocessing = ListField(max_length=10, required=True)
+    model = ListField(max_length=10, required=True)
+    create = DateTimeField(default=datetime.datetime.now)
+    email = StringField(required=True)
+    first_name = StringField(max_length=50)
+
+"""        
 logout = {
-    "topic" : "pyeongyang",
+    "topic" : ,
     "mapper" : {
         "-$type" : "&$logout",
         "-$api_key" : "#SIFTSCIENCE.apikey",
@@ -58,3 +68,77 @@ logout = {
     "is_active" : False
 }
 """
+
+class User(EmbeddedDocument):
+    last_name = StringField(max_length=50)
+
+class Poster(Document):
+    title = StringField(max_length=120, required=True)
+    author = EmbeddedDocumentField(User)
+    tags = ListField(StringField(max_length=30))
+    meta = {'allow_inheritance': True}
+
+class TextPoster(Poster):
+    content = StringField()
+
+class ImagePoster(Poster):
+    image_path = StringField()
+
+class LinkPoster(Poster):
+    link_url = StringField()
+
+if __name__ == "__main__":
+    ross = User(email='jm@example.com')
+    ross.first_name = 'jose'
+    ross.last_name = 'mourinho'
+    ross.save()
+
+    john = User(email='john@example.com')
+    john.first_name = 'John'
+    john.last_name = 'Lawley'
+    john.save()
+
+    query = Q(tags='mongodb')
+    query_result = query.to_query(Poster)
+    print(query_result)
+
+    num_posts = Poster.objects(tags='mongodb')
+    print(num_posts)
+
+    post1 = TextPoster(title='MongoEngine very weird', author=john)
+    post1.content = 'Took a look at MongoEngine today.'
+    post1.tags = ['mongodb', 'mongoal', 'machine-learning']
+    post1.save()
+
+    # extract value from
+    temp = Poster.objects(author__first_name="John")
+    for data in temp:
+        print(data.author.email)
+
+    post2 = LinkPoster(title='MongoEngine Documentation', author=ross)
+    post2.link_url = 'http://docs.mongoengine.com/'
+    post2.tags = ['mongoengine']
+    post2.save()
+
+    for piss in Poster.objects:
+        print(piss.author)
+
+    for post in Poster.objects:
+        print(post.title)
+        print('=' * len(post.title))
+
+        if isinstance(post, TextPoster):
+            print(post.content)
+
+        if isinstance(post, LinkPoster):
+            print('Link: {}'.format(post.link_url))
+
+    post = Poster.objects(tags='mongodb')
+    print(post.title)
+
+# import pickle
+#
+# binary = pickle.dumps(nn)
+# export_file = {"Model": binary}
+# binary_list = list()
+# binary_list.append(binary)
