39c6ff80770bc5a7ab268ee4fe3408f7fd29f61a
falah <falah.fakhruddin@gdn-commerce.com>
2018-03-12 10:37:49 +0700

    resolve

--- app/mlprogram/tests/algorithmtests.py
+++ app/mlprogram/tests/algorithmtests.py
@@ -1,4 +1,9 @@
 import unittest
+import numpy as np
+from ..algorithm.LogisticRegression import LogisticRegression
+from ..algorithm.MLPClassifier import MLPClassifier
+from ..algorithm.NaiveBayess import NaiveBayess
+from ..algorithm.RegressionMainCode import MultiVariateRegression
 
 class AlgorithmTests(unittest.TestCase):
     def test_multiplication(self):
@@ -10,6 +15,16 @@ class AlgorithmTests(unittest.TestCase):
         temp = 2
         result = 5/2
         self.assertEqual(result, temp)
+    
+    def test_sum(self):
+        temp = 3
+        result = 2+1
+        self.assertEqual(result, temp)
 
+    def sigmoid_logistic_regression(self):
+        
+        temp = 1/(1+np.exp(-scores))
+        logreg = LogisticRegression()
+        result = logreg.sigmoid
 if __name__ == "__main__":
     unittest.main()
