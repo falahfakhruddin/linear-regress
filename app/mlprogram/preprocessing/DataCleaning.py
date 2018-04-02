c67f55f9c3338f6b2a4f43abf2ccf7a8891698e6
falah <falah.fakhruddin@gdn-commerce.com>
2018-04-02 17:20:13 +0700

    delete unused code

--- app/mlprogram/preprocessing/DataCleaning.py
+++ app/mlprogram/preprocessing/DataCleaning.py
@@ -10,68 +10,6 @@ from scipy import stats
 from random import randint
 from app.mlprogram.Abstraction import AbstractPreprocessing
 
-class DataCleaning(AbstractPreprocessing):
-      def __init__ (self, missingValues = "NaN", strategy = "mode", axis = 0):
-            self.missingValues = missingValues
-            self.strategy = strategy 
-            self.axis = axis
-            
-      def getMask(self, X, valueMask):
-          """Compute the boolean mask X == missingValues."""
-          if valueMask == "NaN":
-              return np.isnan(X)
-          else:
-              return X == valueMask
-        
-      def fit (self, X):
-
-            #check parameter
-            allowedStrategy = ["mean", "median", "mode"]
-            if self.strategy not in allowedStrategy:
-                  raise ValueError("Can only use these strategies: {0} "
-                             " got strategy={1}".format(allowedStrategy,
-                                                        self.strategy))
-            if self.axis not in [0, 1]:
-                  raise ValueError("Can only impute missing values on axis 0 and 1, "
-                             " got axis={0}".format(self.axis))
-
-            
-            mask = self.getMask(X, self.missingValues)
-            maskedX = ma.masked_array(X, mask=mask)
-                  
-            if self.strategy == "mean":
-                  meanMasked = np.ma.mean(maskedX, axis=self.axis)
-                  self.mean = np.ma.getdata(meanMasked)
-                  #self.mean = np.append(self.mean, [1])
-                  return self.mean
-                        
-            elif self.strategy == "median":
-                   medianMasked = np.ma.median(maskedX, axis=self.axis)
-                   self.median = np.ma.getdata(medianMasked)
-                   #self.median = np.append(self.median, [1])
-                   return self.median
-                      
-            elif self.strategy == "mode":
-
-                   mode = stats.mode(X)
-                   self.mostFrequent = mode[0][0]
-                   #self.mostFrequent = np.append(self.mostFrequent, [1])
-                   return self.mostFrequent  
-                  
-      def transform (self, X):
-            mask = self.getMask(X, self.missingValues)
-            maskedX = ma.masked_array(X, mask=mask)
-            if self.strategy == "mean":
-                filledX = maskedX.filled(self.mean)
-             
-            elif self.strategy == "median":
-                filledX = maskedX.filled(self.median)
-                
-            elif self.strategy == "mode":
-                  filledX = maskedX.filled(self.mostFrequent)
-                
-            return filledX
-
 class DataCleaning2(AbstractPreprocessing):
     def __init__(self, method='mode'):
         self.method = method
